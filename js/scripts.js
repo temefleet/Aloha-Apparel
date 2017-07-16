58adc959e94d6b12b036d78fba7adf4c073c2388
jefe temeso <jefe@fleet.local>
2017-07-15 20:26:45 -0700

    smooth scrolling for all

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,16 @@ $(function() {
     cellAlign: 'left',
     contain: true
   });
+
+  $(".navbar a").not('[href="#"]').click(function() {
+    var sectionId = $(this).attr("href");
+     if ($(window).width() < 600) {	
+		  $("body").animate({scrollTop: $(sectionId).offset().top-115.85}, "slow");
+    }	else if ($(window).width() < 1240) {
+      $("body").animate({scrollTop: $(sectionId).offset().top-84.62}, "slow");
+    } else {
+      $("body").animate({scrollTop: $(sectionId).offset().top-96.5}, "slow");
+    }
+    return false;
+  });
 });
